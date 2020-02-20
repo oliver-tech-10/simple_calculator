8d253a177b55a072381f2321a90a7672e69955fc
oliver-tech-10 <60822835+oliver-tech-10@users.noreply.github.com>
2020-02-20 12:13:07 +0000

    finished run_it (#4)

---
+++ run_it.py
@@ -0,0 +1,19 @@
+from addition import addition
+from multication import multiplication
+from division import division
+from subtraction import subtraction
+
+num_1 = input("Please insert your first number: ")
+num_2 = input("Please insert your second number: ")
+operation = input("Please insert your operation: ")
+
+if operation == "x" or operation == "X" or operation == "*":
+    print(multiplication(num_1, num_2))
+elif operation == "+":
+    print(addition(num_1, num_2))
+elif operation == "-":
+    print(subtraction(num_1, num_2))
+elif operation == "/":
+    print(division(num_1, num_2))
+else:
+    print("You are trying to break my computer.")
